c354693eab3c11cb802e0da2a43185122ac8ecdc
Phi Nguyen <daorong096@gmail.com>
2024-11-10 17:56:27 +0700

    Update images, subjects data, Create CourseNavigator and ProgressNavigator, StudyScreen

--- src/data/Subjects.js
+++ src/data/Subjects.js
@@ -1,4 +1,5 @@
 export const tempSubjects = [
+    // lập trình
     {
         id: 1,
         image: require("../../assets/NMLT.png"),
@@ -8,7 +9,52 @@ export const tempSubjects = [
         DateCreated: "2024-11-04",
         DateUpdated: "2024-11-04",
         courseId: 1,
-        duration: "20 tiếng"
+        duration: "20 tiếng",
+        teachBy: "Mai Tiến Dũng",
+        lessons: [
+            {
+                id: 1,
+                topic: "Giới thiệu",
+                time: 15,
+                data: [
+                    {
+                        id: 1,
+                        title: "Giới thiệu tổng quan",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 5,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Cài đặt môi trường",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 15,
+                        isComplete: false
+                    },
+                ]  
+            },
+            {
+                id: 1,
+                topic: "Thuật toán",
+                time: 100,
+                data: [
+                    {
+                        id: 1,
+                        title: "Khái niệm",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 20,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Bài tập ví dụ",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 80,
+                        isComplete: false
+                    },
+                ]  
+            }
+        ]
     },
     {
         id: 2,
@@ -19,7 +65,52 @@ export const tempSubjects = [
         DateCreated: "2024-11-04",
         DateUpdated: "2024-11-04",
         courseId: 1,
-        duration: "20 tiếng"
+        duration: "20 tiếng",
+        teachBy: "Nguyễn Hữu Lượng",
+        lessons: [
+            {
+                id: 1,
+                topic: "Giới thiệu",
+                time: 15,
+                data: [
+                    {
+                        id: 1,
+                        title: "Giới thiệu tổng quan",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 5,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Cài đặt môi trường",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 15,
+                        isComplete: false
+                    },
+                ]  
+            },
+            {
+                id: 1,
+                topic: "Thuật toán",
+                time: 100,
+                data: [
+                    {
+                        id: 1,
+                        title: "Khái niệm",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 20,
+                        isComplete: false
+                    },
+                    {
+                        id: 2,
+                        title: "Bài tập ví dụ",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 80,
+                        isComplete: false
+                    },
+                ]  
+            }
+        ]
     },
     {
         id: 3,
@@ -30,6 +121,399 @@ export const tempSubjects = [
         DateCreated: "2024-11-04",
         DateUpdated: "2024-11-04",
         courseId: 1,
-        duration: "20 tiếng"
-    }
+        duration: "20 tiếng",
+        teachBy: "Nguyễn Tuấn Nam",
+        lessons: [
+            {
+                id: 1,
+                topic: "Giới thiệu",
+                time: 15,
+                data: [
+                    {
+                        id: 1,
+                        title: "Giới thiệu tổng quan",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 5,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Cài đặt môi trường",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 15,
+                        isComplete: true
+                    },
+                ]  
+            },
+            {
+                id: 1,
+                topic: "Thuật toán",
+                time: 100,
+                data: [
+                    {
+                        id: 1,
+                        title: "Khái niệm",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 20,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Bài tập ví dụ",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 80,
+                        isComplete: false
+                    },
+                ]  
+            }
+        ]
+    },
+    {
+        id: 4,
+        image: require("../../assets/CTDL_GT.png"),
+        title: "Cấu trúc dữ liệu và giải thuật",
+        description: "Môn học sẽ cung cấp kiến thức về Cung cấp cho sinh viên những khái niệm cơ bản về mạng máy tính và truyền dữ liệu trên mạng...",
+        isPublished: true,
+        DateCreated: "2024-11-04",
+        DateUpdated: "2024-11-04",
+        courseId: 1,
+        duration: "20 tiếng",
+        teachBy: "Phạm Thế Sơn",
+        lessons: [
+            {
+                id: 1,
+                topic: "Giới thiệu",
+                time: 15,
+                data: [
+                    {
+                        id: 1,
+                        title: "Giới thiệu tổng quan",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 5,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Cài đặt môi trường",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 15,
+                        isComplete: true
+                    },
+                ]  
+            },
+            {
+                id: 1,
+                topic: "Thuật toán",
+                time: 100,
+                data: [
+                    {
+                        id: 1,
+                        title: "Khái niệm",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 20,
+                        isComplete: false
+                    },
+                    {
+                        id: 2,
+                        title: "Bài tập ví dụ",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 80,
+                        isComplete: false
+                    },
+                ]  
+            }
+        ]
+    },
+
+
+    
+    {
+        id: 5,
+        image: require("../../assets/OOP.png"),
+        title: "Lập Trình Hướng Đối Tượng",
+        description: "Môn học sẽ cung cấp kiến thức về Cung cấp cho sinh viên những khái niệm cơ bản về mạng máy tính và truyền dữ liệu trên mạng...",
+        isPublished: true,
+        DateCreated: "2024-11-04",
+        DateUpdated: "2024-11-04",
+        courseId: 1,
+        duration: "20 tiếng",
+        teachBy: "Mai Tiến Dũng",
+        lessons: [
+            {
+                id: 1,
+                topic: "Giới thiệu",
+                time: 15,
+                data: [
+                    {
+                        id: 1,
+                        title: "Giới thiệu tổng quan",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 5,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Cài đặt môi trường",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 15,
+                        isComplete: true
+                    },
+                ]  
+            },
+            {
+                id: 1,
+                topic: "Thuật toán",
+                time: 100,
+                data: [
+                    {
+                        id: 1,
+                        title: "Khái niệm",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 20,
+                        isComplete: false
+                    },
+                    {
+                        id: 2,
+                        title: "Bài tập ví dụ",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 80,
+                        isComplete: false
+                    },
+                ]  
+            }
+        ]
+    },
+
+
+
+    // toán
+    {
+        id: 6,
+        image: require("../../assets/DSTT.png"),
+        title: "Đại số tuyến tính",
+        description: "Môn học sẽ cung cấp kiến thức về Cung cấp cho sinh viên những khái niệm cơ bản về mạng máy tính và truyền dữ liệu trên mạng...",
+        isPublished: true,
+        DateCreated: "2024-11-04",
+        DateUpdated: "2024-11-04",
+        courseId: 2,
+        duration: "20 tiếng",
+        teachBy: "Nguyễn Minh Trí",
+        lessons: [
+            {
+                id: 1,
+                topic: "Giới thiệu",
+                time: 15,
+                data: [
+                    {
+                        id: 1,
+                        title: "Giới thiệu tổng quan",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 5,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Cài đặt môi trường",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 15,
+                        isComplete: true
+                    },
+                ]  
+            },
+            {
+                id: 1,
+                topic: "Thuật toán",
+                time: 100,
+                data: [
+                    {
+                        id: 1,
+                        title: "Khái niệm",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 20,
+                        isComplete: false
+                    },
+                    {
+                        id: 2,
+                        title: "Bài tập ví dụ",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 80,
+                        isComplete: false
+                    },
+                ]  
+            }
+        ]
+    },
+
+    {
+        id: 7,
+        image: require("../../assets/XSTK.jpg"),
+        title: "Xác suất thống kê",
+        description: "Môn học sẽ cung cấp kiến thức về Cung cấp cho sinh viên những khái niệm cơ bản về mạng máy tính và truyền dữ liệu trên mạng...",
+        isPublished: true,
+        DateCreated: "2024-11-04",
+        DateUpdated: "2024-11-04",
+        courseId: 2,
+        duration: "20 tiếng",
+        teachBy: "Hà Mạnh Linh",
+        lessons: [
+            {
+                id: 1,
+                topic: "Giới thiệu",
+                time: 15,
+                data: [
+                    {
+                        id: 1,
+                        title: "Giới thiệu tổng quan",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 5,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Cài đặt môi trường",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 15,
+                        isComplete: true
+                    },
+                ]  
+            },
+            {
+                id: 1,
+                topic: "Thuật toán",
+                time: 100,
+                data: [
+                    {
+                        id: 1,
+                        title: "Khái niệm",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 20,
+                        isComplete: false
+                    },
+                    {
+                        id: 2,
+                        title: "Bài tập ví dụ",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 80,
+                        isComplete: false
+                    },
+                ]  
+            }
+        ]
+    },
+
+    {
+        id: 8,
+        image: require("../../assets/XSTK.jpg"),
+        title: "Cấu trúc rời rạc",
+        description: "Môn học sẽ cung cấp kiến thức về Cung cấp cho sinh viên những khái niệm cơ bản về mạng máy tính và truyền dữ liệu trên mạng...",
+        isPublished: true,
+        DateCreated: "2024-11-04",
+        DateUpdated: "2024-11-04",
+        courseId: 2,
+        duration: "20 tiếng",
+        teachBy: "Lê Hoàng Tuấn",
+        lessons: [
+            {
+                id: 1,
+                topic: "Giới thiệu",
+                time: 15,
+                data: [
+                    {
+                        id: 1,
+                        title: "Giới thiệu tổng quan",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 5,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Cài đặt môi trường",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 15,
+                        isComplete: true
+                    },
+                ]  
+            },
+            {
+                id: 1,
+                topic: "Thuật toán",
+                time: 100,
+                data: [
+                    {
+                        id: 1,
+                        title: "Khái niệm",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 20,
+                        isComplete: false
+                    },
+                    {
+                        id: 2,
+                        title: "Bài tập ví dụ",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 80,
+                        isComplete: false
+                    },
+                ]  
+            }
+        ]
+    },
+
+    {
+        id: 9,
+        image: require("../../assets/GT.png"),
+        title: "Giải tích",
+        description: "Môn học sẽ cung cấp kiến thức về Cung cấp cho sinh viên những khái niệm cơ bản về mạng máy tính và truyền dữ liệu trên mạng...",
+        isPublished: true,
+        DateCreated: "2024-11-04",
+        DateUpdated: "2024-11-04",
+        courseId: 2,
+        duration: "20 tiếng",
+        teachBy: "Cao Thanh Tình",
+        lessons: [
+            {
+                id: 1,
+                topic: "Giới thiệu",
+                time: 15,
+                data: [
+                    {
+                        id: 1,
+                        title: "Giới thiệu tổng quan",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 5,
+                        isComplete: true
+                    },
+                    {
+                        id: 2,
+                        title: "Cài đặt môi trường",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 15,
+                        isComplete: true
+                    },
+                ]  
+            },
+            {
+                id: 1,
+                topic: "Thuật toán",
+                time: 100,
+                data: [
+                    {
+                        id: 1,
+                        title: "Khái niệm",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 20,
+                        isComplete: false
+                    },
+                    {
+                        id: 2,
+                        title: "Bài tập ví dụ",
+                        videoId: "1nQe2ZiwWFw",
+                        time: 80,
+                        isComplete: false
+                    },
+                ]  
+            }
+        ]
+    },
+
+
 ]
---
+++ src/screens/StudyScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import BackButton from '../components/BackButton';
+
+export default function StudyScreen({ route }) {
+    // Lấy dữ liệu từ params
+    const { examData } = route.params || {};
+
+    return (
+      <SafeAreaProvider>
+          <SafeAreaView style={styles.container}>
+            <BackButton />
+            <Text style={styles.headerTitle}>Study Screen</Text>
+          </SafeAreaView>
+        </SafeAreaProvider>
+    );
+}
+  
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    scrollView: {
+      // flex: 1,
+    },
+    image: {
+      width: '100%',
+      height: undefined,
+      aspectRatio: 0.85,
+      marginVertical: 10,
+      resizeMode: "contain"
+    },
+    headerTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#2282FE',
+      marginVertical: 20,
+      marginHorizontal: 20,
+      marginTop: 30,
+      textAlign: 'center'
+    },
+  });
+  
